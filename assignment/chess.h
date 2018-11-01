41236b4c081419bc5fe00f4528e658fde7bd7443
LeonNepelius <leon@nepelius.at>
2018-11-01 19:11:57 +0100

    Managed to compile

--- assignment/chess.h
+++ assignment/chess.h
@@ -1,16 +1,18 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: 2AHDV
+ *				HTBLA-Leonding / Klasse: 2DHIF
  * ---------------------------------------------------------
  * Exercise Number: 0
  * Title:			chess.h
- * Author:			P. Bauer
- * Due Date:		October 15, 2014
+ * Author:			Leon Nepelius
+ * Due Date:		November 2, 2018
  * ----------------------------------------------------------
  * Description:
  * Basic chess functions.
  * ----------------------------------------------------------
  */
  #include <stdbool.h>
+#ifndef ___CHESS_H
+#define ___CHESS_H
 enum Color {White,Black};
 enum PieceType
 {
@@ -35,8 +37,7 @@ struct ChessSquare
 
 typedef int Rank;
 typedef char File;
-typedef ChessSquare ChessBoard[8][8];
-typedef ChessSquare Chessboard[8][8];
+typedef struct ChessSquare ChessBoard[8][8];
 
 
 struct ChessPiece get_piece(ChessBoard chess_board, File file , int rank)
@@ -50,7 +51,7 @@ bool is_piece(struct ChessPiece piece,enum Color color,enum PieceType type)
 	return piece.type == type && piece.color == color;
 }
 
-void init_chess_board(Chessboard chess_board)
+void init_chess_board(ChessBoard chess_board)
 {
 	for(int i=0;i<8;i++)
 	{
@@ -60,7 +61,7 @@ void init_chess_board(Chessboard chess_board)
 		}
 	}
 }
-void setup_chess_board(Chessboard chess_board);
+void setup_chess_board(ChessBoard chess_board);
 
 bool is_square_occupied(ChessBoard chess_board,File file,Rank rank);
 struct ChessSquare* get_square(ChessBoard chess_board,File file, Rank rank);
@@ -74,5 +75,6 @@ bool squares_share_diagonal(File file1, Rank rank1, File file2, Rank rank2);
 
 bool squares_share_knights_move(File file1, Rank rank1, File file2, Rank rank2);
 bool squares_share_kings_move(File file1, Rank rank1, File file2, Rank rank2);
-bool squares_share_pawns_move(enum Color color,enum PawnMove, File file1, Rank rank1, File file2, Rank rank2);
+bool squares_share_pawns_move(enum Color color,enum PawnMove move, File file1, Rank rank1, File file2, Rank rank2);
 bool squares_share_queens_move(File file1, Rank rank1, File file2, Rank rank2);
+#endif
